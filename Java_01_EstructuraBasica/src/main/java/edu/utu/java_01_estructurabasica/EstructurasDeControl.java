51ac7646f83022843118ae940a4ba4a1793f0b95
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2020-12-04 16:05:58 -0300

    Nuevo Código

---
+++ Java_01_EstructuraBasica/src/main/java/edu/utu/java_01_estructurabasica/EstructurasDeControl.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.utu.java_01_estructurabasica;
+
+/***
+ * NOTA: No se supone que este código sea compilable (de hecho, no lo es), su
+ * único propósito es demostrar la sintaxis de Java.
+ * @author Luis Sebastián de los Ángeles
+ */
+public class EstructurasDeControl {
+ 
+    public void Estructuras(){
+    
+        /***
+        * Estructura Condicional (IF): Permite que el programa controle el flujo
+        * de la ejecución dependiendo del valor (verdadero o falso) que tenga el 
+        * chequeo de una condición. 
+        * Las condiciones por lo general toman la forma de comparaciones.
+        */
+        
+        if ( condicion ) {
+            // Si la condición devuelve el valor VERDADERO, se ejecutan las sentencias
+            //de este bloque.
+        }
+        else{
+            // Si la condición devuelve el valor FALSO, se ejecutan las sentencias
+            //de este bloque.
+        }
+        
+        
+    }
+    
+    
+    
+}
