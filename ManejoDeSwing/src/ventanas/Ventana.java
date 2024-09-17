3684c0c53e5556e8201356ba9648da11e9c4c372
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2024-09-17 11:29:01 -0300

    Ultimos cambios previos a commit

--- ManejoDeSwing/src/ventanas/Ventana.java
+++ ManejoDeSwing/src/ventanas/Ventana.java
@@ -2,20 +2,21 @@
 package ventanas;
 
 import java.awt.Dimension;
+import javax.swing.JFrame;
 
 /**
  *
  * @author Luis Sebastián de los Ángeles
  */
-public class Ventana extends javax.swing.JFrame{
+public class Ventana {
     
     /**
      * Método para inicializar los componentes de la ventana
      */
-    private void iniciarComponentes(){
+    private void iniciarComponentes(JFrame ventana){
         
         //Acción al cerrar la ventana
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         //Determinación del tamaño inicial de la ventana:
         //1 - Dimensiones establecidas
